2bd1e2b5e38affcb44d5deb77a0d53bd60b9f627
gmjplayys@gmail.com <gmjplayys@gmail.com>
2023-02-03 08:40:03 +0200

    refactored java file

--- script/Util.java
+++ script/Util.java
@@ -4,19 +4,21 @@ import java.io.File;
 
 public class Util
 {
+  private static final String HOME = "/home/runne/work/gabrielmjr/gabrielmjr";
+  
   private static File old_readme_file;
   private static File new_readme_file;
   
   
   public static void main(String[] args)
   {
-    old_readme_file = new File("/home/runner/work/gabrielmjr/gabrielmjr/README.html");
+    old_readme_file = new File(HOME + "/README.html");
     
     if (old_readme_file.exists())
       System.out.println("File already exists...");
       System.exit(1);
     
-    new_readme_file = new File("/home/runner/work/gabrielmjr/gabrielmjr/README.md");
+    new_readme_file = new File(HOME + "/README.md");
     
     if (!renameReadmeFile())
       System.out.println("File not renamed");
